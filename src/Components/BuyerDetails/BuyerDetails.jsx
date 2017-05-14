8d84ba391beedfc501abcf5b4fe4d13ec4f85766
Hugh Loughrey <hugh.loughrey@gmail.com>
2017-05-14 22:06:57 +0100

    Add Buyer Details Show / Hide

--- src/Components/BuyerDetails/BuyerDetails.jsx
+++ src/Components/BuyerDetails/BuyerDetails.jsx
@@ -4,37 +4,62 @@ import './BuyerDetails.scss';
 export default class BuyerDetails extends Component {
   constructor() {
     super()
+
+    this.state = {
+      buyerDetails: {
+        visible: true
+      }
+    };
+
+    this.detailsVisibility = this.detailsVisibility.bind(this);
+  }
+
+  detailsVisibility(e) {
+    this.setState({
+      buyerDetails: {
+        visible: !this.state.buyerDetails.visible
+      }
+    });
   }
 
   render() {
-    return (
-      <div className='col-md-12'>
-        <h2 className='primary'>Buyer Details</h2>
-        <p className='pull-right'>hide</p>
-        <hr />
-        <div className='buyers-details-table'>
-          <table className='table'>
-            <tbody>
-              <tr>
-                <th className='primary'>Name:</th>
-                <td>Mr David Shepard</td>
-              </tr>
-              <tr>
-                <th className='primary'>Buyer position:</th>
-                <td>Property to set - 20 weeks on the market</td>
-              </tr>
-              <tr>
-                <th className='primary'>Finance poition:</th>
-                <td>Mortgage Required - approved</td>
-              </tr>
-              <tr>
-                <th className='primary'>Timescale:</th>
-                <td>Would like to move in 10 weeks - no chain</td>
-              </tr>
-            </tbody>
-          </table>
+    if(this.state.buyerDetails.visible) {
+      return (
+        <div className='col-md-12 buyer-details'>
+          <h2 className='primary'>Buyer Details</h2>
+          <p className='primary' onClick={this.detailsVisibility}>HIDE <span className='glyphicon glyphicon-chevron-up'></span></p>
+          <hr />
+          <div id='buyers-details-table' className='buyers-details-table'>
+            <table className='table'>
+              <tbody>
+                <tr>
+                  <th className='primary'>Name:</th>
+                  <td>Mr David Shepard</td>
+                </tr>
+                <tr>
+                  <th className='primary'>Buyer position:</th>
+                  <td>Property to set - 20 weeks on the market</td>
+                </tr>
+                <tr>
+                  <th className='primary'>Finance poition:</th>
+                  <td>Mortgage Required - approved</td>
+                </tr>
+                <tr>
+                  <th className='primary'>Timescale:</th>
+                  <td>Would like to move in 10 weeks - no chain</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className='col-md-12 buyer-details'>
+          <h2 className='primary'>Buyer Details</h2>
+          <p className='primary' onClick={this.detailsVisibility}>SHOW <span className='glyphicon glyphicon-chevron-up'></span></p>
+        </div>
+      )
+    }
   }
 }
